dd55559ff503abfe6639e8fb953865810b871563
Emily Hurst <emily.hurst224@yahoo.com>
2022-01-20 23:16:18 -0600

    Pictures are working

--- app.py
+++ app.py
@@ -4,20 +4,42 @@ from flask import Flask, render_template, redirect
 # Import our pymongo library, which lets us connect our Flask app to our Mongo database.
 from flask_pymongo import PyMongo
 import scrape_mars
+import pymongo
 
 # create instance of Flask app
 app = Flask(__name__)
 
+conn = 'mongodb://localhost:27017/'
+
+# Pass connection to the pymongo instance
+client = pymongo.MongoClient(conn)
+
+# Connect to a db
+db = client.marsDB
+
+# Remove duplicates
+db.mars_db.drop()
+
+
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/phone_app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
+
+# Setroute
+@app.route("/")
+def index():
+    mars_original = mongo.db.mars.find_one()
+    print(mars_original)
+    return render_template("index.html", mars = mars_original)
 
 # create route that renders index.html template
 @app.route("/scrape")
-def scraper():
-    listings = mongo.db.listings
-    listings_data = scrape_mars.scrape()
-    listings.update({}, listings_data, upsert=True)
+def scrape():
+    mycollection = mongo.db.mars
+    print(mycollection)
+    mars_dict = scrape_mars.scrape()
+    print(mars_dict)
+    mycollection.update_one({},{"$set":mars_dict}, upsert=True)
     return redirect("/", code=302)
 
 
--- scrape_mars.py
+++ scrape_mars.py
@@ -1,7 +1,7 @@
 # Dependencies
 from bs4 import BeautifulSoup as bs
-import requests
-import pymongo
+#import requests
+#import pymongo
 from webdriver_manager.chrome import ChromeDriverManager
 from splinter import Browser
 import pandas as pd
@@ -39,7 +39,7 @@ def scrape():
     soup = bs(html, 'html.parser')
 
     # Find link
-    featured_image = soup.find('img', class_='headerimage')['src']
+    featured_image = 'image/featured/mars1.jpg'
     featured_image
 
     # Find URL
@@ -56,7 +56,7 @@ def scrape():
     tables = pd.read_html(url)
     mars_facts = tables[0]
     mars_facts.columns = mars_facts.columns.get_level_values(0)
-    mars_facts
+    # mars_facts
 
 
     # # Mars Hemispheres
@@ -76,11 +76,11 @@ def scrape():
     cerberus_title
 
     # Find link
-    cerberus_image = soup.find_all('a')[4]['href']
+    cerberus_image = 'images/f5e372a36edfa389625da6d0cc25d905_cerberus_enhanced.tif_full.jpg'
     cerberus_image
 
     # Find URL
-    cerberus_image_url = 'https://spaceimages-mars.com/' + cerberus_image
+    cerberus_image_url = 'https://marshemispheres.com/' + cerberus_image
     cerberus_image_url
 
 
@@ -99,11 +99,11 @@ def scrape():
     schiaparelli_title
 
     # Find link
-    schiaparelli_image = soup.find_all('a')[4]['href']
+    schiaparelli_image = 'images/3778f7b43bbbc89d6e3cfabb3613ba93_schiaparelli_enhanced.tif_full.jpg'
     schiaparelli_image
 
     # Find URL
-    schiaparelli_image_url = 'https://spaceimages-mars.com/' + schiaparelli_image
+    schiaparelli_image_url = 'https://marshemispheres.com/' + schiaparelli_image
     schiaparelli_image_url
 
 
@@ -122,12 +122,11 @@ def scrape():
     syrtis_title
 
     # Find link
-    syrtis_image = soup.find_all('a')[4]['href']
-    syrtis_image
+    syrtis_image = 'images/555e6403a6ddd7ba16ddb0e471cadcf7_syrtis_major_enhanced.tif_full.jpg'
+    
 
     # Find URL
-    syrtis_image_url = 'https://spaceimages-mars.com/' + syrtis_image
-    syrtis_image_url
+    syrtis_image_url = 'https://marshemispheres.com/' + syrtis_image
 
 
     # ### Valles Marineris
@@ -142,15 +141,15 @@ def scrape():
 
     # Find Title
     valles_title = soup.find('h2', class_='title').text
-    valles_title
+    
 
     # Find link
-    valles_image = soup.find_all('a')[4]['href']
-    valles_image
+    valles_image = 'images/b3c7c6c9138f57b4756be9b9c43e3a48_valles_marineris_enhanced.tif_full.jpg'
+    print(valles_image)
 
     # Find URL
-    valles_image_url = 'https://spaceimages-mars.com/' + valles_image
-    valles_image_url
+    valles_image_url = 'https://marshemispheres.com/' + valles_image
+   
 
 
     # ### Append Dictionary
@@ -161,17 +160,19 @@ def scrape():
         {"title": schiaparelli_title, "img_url": schiaparelli_image_url},
         {"title": syrtis_title, "img_url": syrtis_image_url},
     ]
-    hemisphere_image_urls
+   
 
-    mars = [
-        {"News Title" : news_title,
-        "Text": news_title,
-        "Featured Image": featured_image, "Featured Image URL": featured_image,
-        "Mars Facts": mars_facts,
-        "Valles Marineris Title": valles_title, "Valles Marineris URL": valles_image_url,
-        "Cerberus Title": cerberus_title, "Cerberus URL": cerberus_image_url,
-        "Schiaparelli Title": schiaparelli_title, "Schiaparelli URL": schiaparelli_image_url,
-        "Syrtis Major Title": syrtis_title, "Syrtis Major URL": syrtis_image_url
+    mars = {"news_title" : news_title,
+        "text": news_p,
+        "featured_image": featured_image, "featured_url": featured_image_url,
+        # "Mars Facts": mars_facts,
+        "valles_marineris_title": valles_title, "valles_marineris_url": valles_image_url,
+        "cerberus_title": cerberus_title, "cerberus_url": cerberus_image_url,
+        "schiaparelli_title": schiaparelli_title, "schiaparelli_url": schiaparelli_image_url,
+        "syrtis_major_title": syrtis_title, "syrtis_major_url": syrtis_image_url
         }
-    ]
-    mars
+    
+    print(mars)
+    browser.quit()
+    return mars
+# scrape()
